ee1d0d3794b22330618f630168972d506185fa03
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-06-20 17:24:25 -0500

    completed styling of registration page and functionality is working. Registration is now able to be integrated into the backend

--- HTML/assets/js/registration.js
+++ HTML/assets/js/registration.js
@@ -1,17 +1,20 @@
+document.body.innerHTML = RegisterView();
+RegisterEvent();
+
 function RegisterView(){
     //language=HTML
     return`
      <div class="container">
         <form id="form1">
             <h3>Create Account</h3>
-            <input type="fname" placeholder="First Name">
-            <input type="lname" placeholder="Last Name">
-            <input type="DOB" placeholder="DOB (format YYYY-MM-DD)">
-            <input type="SSN" placeholder="SSN (format ###-##-####)">
-            <input type="text" placeholder="Email">
-            <input type="username" placeholder="Enter your desired username">
-            <input type="password" placeholder="Password">
-            <input type="password" placeholder="Type your password again">
+            <input type="fname" placeholder="First Name" required>
+            <input type="lname" placeholder="Last Name" required>
+            <input type="DOB" placeholder="DOB (format YYYY-MM-DD)" required>
+            <input type="SSN" placeholder="SSN (format ###-##-####)" required>
+            <input type="text" placeholder="Email" required>
+            
+            <br>
+            <br>
             <br>
             <br>
             <div class="btn-box">
@@ -20,9 +23,17 @@ function RegisterView(){
         </form>
         <!--Form page 3-->
         <form id="form2">
-            <h3>Veteran Verification</h3>
             <span class="loader"></span>
-
+            <h3>Veteran Status - You are approved!</h3>
+            
+            <br>
+            <input type="username" placeholder="Enter your desired username">
+            <input type="password" placeholder="Password">
+            <input type="password" placeholder="Confirm Password">
+            <br>
+            <br>
+            <br>
+            <br>
             <div class="btn-box">
                 <button type="button" id="back1" class="submit btn-primary"><<< Back</button>
                 <button type="button" id="next2" class="submit btn-primary">Next>>></button>
@@ -31,24 +42,18 @@ function RegisterView(){
         <!--Form page 4-->
         <form id="form3">
             <h3>Verification Status - Approved</h3>
-            <h1>You have been vetted and approved! Please select your desired communities.</h1>
+            <h5>You have been vetted and approved! Please select your desired communities:</h5>
             <div class="community-container">
                 <ul class="ks-cboxtags">
                     <li><input type="checkbox" id="checkboxOne" value="Rainbow Dash"><label for="checkboxOne">PTSD</label></li>
                     <li><input type="checkbox" id="checkboxTwo" value="Cotton Candy" checked><label for="checkboxTwo">Anxiety</label></li>
                     <li><input type="checkbox" id="checkboxThree" value="Rarity" checked><label for="checkboxThree">Depression</label></li>
-                    <li><input type="checkbox" id="checkboxFour" value="Moondancer"><label for="checkboxFour">Substance Misuse</label></li>
-                    <li><input type="checkbox" id="checkboxFive" value="Surprise"><label for="checkboxFive">Sexual Addiction</label></li>
-                    <li><input type="checkbox" id="checkboxSix" value="Twilight Sparkle" checked><label for="checkboxSix">General Mental Health</label></li>
-                    <li><input type="checkbox" id="checkboxSeven" value="Fluttershy"><label for="checkboxSeven">Eating Disorder</label></li>
-                    <li><input type="checkbox" id="checkboxEight" value="Derpy Hooves"><label for="checkboxEight">Personality Disorder</label></li>
-                    <li><input type="checkbox" id="checkboxNine" value="Princess Celestia"><label for="checkboxNine">Mood Disorder</label></li>
-                    <li><input type="checkbox" id="checkboxTen" value="Gusty"><label for="checkboxTen">Phobias</label></li>
-                    <li class="ks-selected"><input type="checkbox" id="checkboxEleven" value="Discord"><label for="checkboxEleven">Obsessive Compulsive Disorder</label></li>
-                    <li><input type="checkbox" id="checkboxTwelve" value="Clover"><label for="checkboxTwelve">Panic Disorder</label></li>
+                    <li><input type="checkbox" id="checkboxFour" value="Moondancer"><label for="checkboxFour">Alcohol Abuse</label></li>
+                    <li><input type="checkbox" id="checkboxFive" value="Surprise"><label for="checkboxFive">Addiction</label></li>
                 </ul>
             </div>
-            <h4>By click submit, you are agreeing to Vetted <a href="https://benice.com/">community guidelines.</a></h4>
+            <h5>By click submit, you are agreeing to Vetted <a href="https://benice.com/">community guidelines.</a></h5>
+            <br>
             <div class="btn-box">
                 <button type="button" id="back2" class="submit btn-primary"><<< Back</button>
                 <button type="button" class="submit btn-primary">Submit>>></button>
@@ -71,27 +76,27 @@ function RegisterEvent(){
 
     let progress = document.getElementById("progress");
 
-    $(document).on('click', '.next1', function () {
-        form1.style.left = "-450px";
-        form2.style.left = "40px";
+    $(document).on('click', '#next1', function () {
+        form1.style.display = "none";
+        form2.style.display = "block";
         progress.style.width = "240px";
     })
 
-    $(document).on('click', '.back1', function () {
-        form1.style.left = "40px";
-        form2.style.left = "450px";
+    $(document).on('click', '#back1', function () {
+        form1.style.display = "none";
+        form2.style.display = "block";
         progress.style.width = "120px";
     })
 
-    $(document).on('click', '.next2', function () {
-        form2.style.left = "-450px";
-        form3.style.left = "40px";
+    $(document).on('click', '#next2', function () {
+        form2.style.display = "none";
+        form3.style.display = "block";
         progress.style.width = "360px";
     })
 
-    $(document).on('click', '.back2', function () {
-        form1.style.left = "40px";
-        form2.style.left = "450px";
+    $(document).on('click', '#back2', function () {
+        form1.style.display = "none";
+        form2.style.display = "block";
         progress.style.width = "240px";
     })
 }
