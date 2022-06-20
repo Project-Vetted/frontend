a9b6516546c68db97893d1e133b8bba4828ef45e
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-06-20 15:47:47 -0500

    moved jQuery to html file and removed required from first form

---
+++ HTML/assets/js/registration.js
@@ -0,0 +1,33 @@
+
+// function nextForm() {
+//     $(document).on('click', '.next1', function (){
+//         form1.style.left = "-450px";
+//         form2.style.left = "40px";
+//         progress.style.width = "240px";
+//     })
+// }
+//
+// function backForm(){
+//     $(document).on('click', '.back1', function (){
+//         form1.style.left = "40px";
+//         form2.style.left = "450px";
+//         progress.style.width = "120px";
+//     })
+// }
+//
+// function nextForm2() {
+//     $(document).on('click', '.next2', function (){
+//         form2.style.left = "-450px";
+//         form3.style.left = "40px";
+//         progress.style.width = "360px";
+//     })
+// }
+//
+// function backForm2(){
+//     $(document).on('click', '.back1', function (){
+//         form1.style.left = "40px";
+//         form2.style.left = "450px";
+//         progress.style.width = "240px";
+//     })
+// }
+//
