799e3c9aaf2c36ada60b9b9859540c66d39bae89
Mersedyes Livingston <98776643+Mersedyes@users.noreply.github.com>
2022-06-21 00:30:34 -0500

    added toggle functionality to icons, created card skeletons for mental health articles.

--- HTML/assets/js/dashboard2.js
+++ HTML/assets/js/dashboard2.js
@@ -1,5 +1,38 @@
 //Toggle mobile menu
+const menuToggleOpen = document.querySelector('.menu-toggle-open');
+const menuToggleClose = document.querySelector('.menu-toggle-close');
+const navMobileEl = document.querySelector('.nav-mobile');
+
+
+menuToggleOpen.addEventListener('click', () => navMobileEl.classList.add('active'));
+menuToggleClose.addEventListener('click', () => navMobileEl.classList.remove('active'));
 
 //Toggle theme
+const bodyEl = document.body;
+const themeToggleBtn = document.querySelector('.theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme');
+
+
+if(currentTheme){
+    bodyEl.classList.add('theme-light');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+   bodyEl.classList.toggle('theme-light');
+   if (bodyEl.classList.contains('theme-light')){
+       localStorage.setItem('currentTheme', 'themeActive');
+   } else{
+       localStorage.removeItem('currentTheme', 'themeActive');
+   }
+});
 
 //Show dropdown menu
+document.addEventListener('click', element => {
+    const dropdown = document.querySelector('.dropdown');
+
+    if(element.target.classList.contains('dropdown-btn')){
+        dropdown.classList.add('active');
+    } else{
+        dropdown.classList.remove('active');
+    }
+});
